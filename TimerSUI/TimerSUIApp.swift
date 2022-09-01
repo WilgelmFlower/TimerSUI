026e27ded5e0ca2ece05b72e6a746a2eccfabaa0
Mark Wilgelm <markujke@mail.ru>
2022-09-01 19:51:08 +0200

    Initial Commit

---
+++ TimerSUI/TimerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSUIApp.swift
+//  TimerSUI
+//
+//  Created by Mark Wilhelm on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
